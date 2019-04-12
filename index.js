0649740587b5b1a17c0f5783703a01049542c26a
Jackie Tian <zhonghua2412@gmail.com>
2019-04-12 11:56:11 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ const ShoppingCart = require('./src/ShoppingCart');
 const products = require('./src/data/data');
 const priceConfig = require('./priceConfig');
 
-const checkout = () => {
-    /**
-     * receive your input from terminal
-     */
+/**
+ * read user input from terminal
+ */
+const readUserInput = (cb) => {
     process.stdout.write("Scanned SKUs: ")
     const stdin = process.openStdin();
     stdin.addListener("data", function (data) {
-        const skus = data.toString().replace(/ /g, '').trim().split(',');
-        const shoppingCart = new ShoppingCart(products, priceConfig);
-        skus.forEach((sku) => shoppingCart.scan(sku));
-        process.stdout.write(`Total is $${shoppingCart.total()} \n`);
+        cb(data);
+        // waiting for next input
         process.stdout.write("Scanned SKUs: ");
     });
 }
 
-checkout();
+/**
+ * parse user inputs and add skus to shopping cart
+ * @param String data
+ */
+const addToShoppingCart = (data) => {
+    // parse user input, e.g. atv,atv,ipd
+    const skus = data.toString().replace(/ /g, '').trim().split(',');
+    const shoppingCart = new ShoppingCart(products, priceConfig);
+    // scan each into shopping cart
+    skus.forEach((sku) => shoppingCart.scan(sku));
+    // calculate the total price
+    process.stdout.write(`Total is $${shoppingCart.total()} \n`);
+}
+
+readUserInput(addToShoppingCart);
